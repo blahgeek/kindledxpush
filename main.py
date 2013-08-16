e1c012ffb4eec41ffbade2cf6720fbdd62b36fc9
blahgeek <blahgeek@gmail.com>
2013-08-16 15:57:14 +0800

    add some info

--- main.py
+++ main.py
@@ -29,6 +29,7 @@ def getFormHiddenData(form):
     return ret
 
 def login(email, password):
+    logging.info('Login...')
     LOGIN_URL = 'https://www.amazon.com/ap/signin?_encoding=UTF8&openid.assoc_handle=usflex&openid.claimed_id=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.identity=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0%2Fidentifier_select&openid.mode=checkid_setup&openid.ns=http%3A%2F%2Fspecs.openid.net%2Fauth%2F2.0&openid.ns.pape=http%3A%2F%2Fspecs.openid.net%2Fextensions%2Fpape%2F1.0&openid.pape.max_auth_age=0&openid.return_to=https%3A%2F%2Fwww.amazon.com%2Fgp%2Fyourstore%2Fhome%3Fie%3DUTF8%26ref_%3Dgno_signin'
     LOGIN_POST_URL = 'https://www.amazon.com/ap/signin'
     login_page = BeautifulSoup(session.get(LOGIN_URL).text)
@@ -51,6 +52,7 @@ def deliverContent(content):
     assert req.json()['data'] == 1
 
 def deliverAll(contents):
+    logging.info('Delivering...')
     def contentInDB(content):
         try:
             cursor.execute('select * from content where name = "%s"' % content)
